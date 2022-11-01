c0216dc68701316a74f02e08ad150e52b30a0c44
sococa <pierrocarlier@gmail.com>
2022-11-01 20:01:30 +0100

    some animations of services page

--- scripts.js
+++ scripts.js
@@ -46,3 +46,27 @@ function sendEmail(){
         message => alert(message)
     )
 }
+
+/* services page */
+const img1 = document.getElementsByClassName('img1')
+const img2 = document.getElementsByClassName('img2')
+const img3 = document.getElementsByClassName('img3')
+const img4 = document.getElementsByClassName('img4')
+const img5 = document.getElementsByClassName('img5')
+const img6 = document.getElementsByClassName('img6')
+
+const paragraphe1 = document.getElementsByClassName('paragraphe1')
+const paragraphe2 = document.getElementsByClassName('paragraphe2')
+const paragraphe3 = document.getElementsByClassName('paragraphe3')
+const paragraphe4 = document.getElementsByClassName('paragraphe4')
+const paragraphe5 = document.getElementsByClassName('paragraphe5')
+const paragraphe6 = document.getElementsByClassName('paragraphe6')
+const h3 = document.getElementsByTagName('h3')
+console.log(h3)
+
+img1.addEventListener(
+    "mouseover", 
+    function(e){
+        e.style.color = 'red'
+    }
+)
