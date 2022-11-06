5728fab1ea7c1ff81e7b371937f68351df064999
sococa <pierrocarlier@gmail.com>
2022-11-06 22:42:12 +0100

    on hover effect smartphone version my woprk page

--- scripts.js
+++ scripts.js
@@ -1,48 +1,61 @@
 /* Intersection observer */
-const navItems = document.querySelectorAll(".nav_item")
-let formerId = 0
+const navItems = document.querySelectorAll(".nav_item");
+let formerId = 0;
 
 const options = {
     root: null,
     rootMargin: '0px',
     threshold: 0.6
-}
+};
 
-const observer = new IntersectionObserver(handleObserver, options)
+const observer = new IntersectionObserver(handleObserver, options);
 
-const targets = document.querySelectorAll(".slider_bloc")
+const targets = document.querySelectorAll(".slider_bloc");
 
 targets.forEach(target => {
-    observer.observe(target)
+    observer.observe(target);
 })
 
 const underline = id => {
-    navItems[formerId].classList.remove('displayed')
-    navItems[id].classList.add('displayed')
-    formerId = id
+    navItems[formerId].classList.remove('displayed');
+    navItems[id].classList.add('displayed');
+    formerId = id;
 }
 
 function handleObserver(entries, observer) {
     entries.forEach(entry => {
-        entry.isIntersecting && underline(entry.target.dataset.id)
+        entry.isIntersecting && underline(entry.target.dataset.id);
     })
 }
 
+/* My work page */
+
+// let appearOnHover = document.querySelectorAll(".appear_on_hover");
+
+// document.getElementById("test").addEventListener(
+//     "click",
+//     function(){
+
+//     }
+// );
+
 /* smtp js */
-function sendEmail(){
+// function sendEmail(){
 
-    const email = document.getElementsByClassName('email')
-    const message = document.getElementsByClassName('message')
+//     const email = document.getElementsByClassName('email')
+//     const message = document.getElementsByClassName('message')
 
-    Email.send({
-        Host: "smtp.elasticemail.com",
-        Username: "pierrocarlier@gmail.com",
-        Password: "DF3F9E88F1C9662578AA16EE8C6C6B7606C8",
-        To: email.value,
-        From: "pierrocarlier@gmail.com",
-        Subject: "This is the subject",
-        Body: message.value
-    }).then(
-        message => alert(message)
-    )
-}
+//     Email.send({
+//         Host: "smtp.elasticemail.com",
+//         Username: "pierrocarlier@gmail.com",
+//         Password: "DF3F9E88F1C9662578AA16EE8C6C6B7606C8",
+//         To: email.value,
+//         From: "pierrocarlier@gmail.com",
+//         Subject: "This is the subject",
+//         Body: message.value
+//     }).then(
+//         message => alert(message)
+//     )
+// }
+
+
