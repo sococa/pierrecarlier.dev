b2371ac62b6d2571782c9894161c76cf63463423
sococa <pierrocarlier@gmail.com>
2022-11-21 16:00:05 +0100

    add english and french versions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/* Intersection observer */
+/* Gestion du surlignage de la navbar */
 const navItems = document.querySelectorAll(".nav_item");
 let formerId = 0;
 
@@ -8,12 +8,12 @@ const options = {
     threshold: 0.6
 };
 
-const observer = new IntersectionObserver(handleObserver, options);
+const observerForNavbar = new IntersectionObserver(handleObserverForNavbar, options);
 
 const targets = document.querySelectorAll(".slider_bloc");
 
 targets.forEach(target => {
-    observer.observe(target);
+    observerForNavbar.observe(target);
 })
 
 const underline = id => {
@@ -22,7 +22,7 @@ const underline = id => {
     formerId = id;
 }
 
-function handleObserver(entries, observer) {
+function handleObserverForNavbar(entries, observerForNavbar) {
     entries.forEach(entry => {
         entry.isIntersecting && underline(entry.target.dataset.id);
     })
@@ -31,7 +31,7 @@ function handleObserver(entries, observer) {
 
 /* Gestion de la arrow_top */
 /* La flèche apparait quand la photo d'accueil n'est plus visible */
-var observer_arrow = new IntersectionObserver(function(entries) {
+var observerForArrow = new IntersectionObserver(function(entries) {
     if(entries[0].isIntersecting === true){
         document.getElementById("arrow_go_to_top").style.display = "none"
     }
@@ -42,26 +42,44 @@ var observer_arrow = new IntersectionObserver(function(entries) {
     threshold: [1] 
 })
 
-observer_arrow.observe(document.querySelector("h1"))
+observerForArrow.observe(document.querySelector(".first_part"))
 
 
 /* Version anglaise ou française */
+let frenchSelected = document.getElementById('french_selected')
+
+let frenchText = document.getElementsByClassName('french_text')
+let englishText = document.getElementsByClassName('english_text')
+
+let frenchFlagImg = document.getElementById('french_flag_img')
+let englishFlagImg = document.getElementById('english_flag_img')
+
 function languageSelected() {
-
-    let frenchSelected = document.getElementById('french_selected')
-    let frenchText = document.getElementsByClassName('french_text')
-    let englishText = document.getElementsByClassName('english_text')
-
     let n = -1
     while (n < frenchText.length || n < englishText.length) {
         n++
         if(frenchSelected.checked === true) {
             frenchText[n].style.display = "block"
             englishText[n].style.display = "none"
+
+            frenchFlagImg.style.display = "none"
+            englishFlagImg.style.display = "block"
         }
         else {
             englishText[n].style.display = "block"
             frenchText[n].style.display = "none"
+
+            englishFlagImg.style.display = "none"
+            frenchFlagImg.style.display = "block"
         }
     }
+}
+
+if(englishSelected.checked === true){
+    frenchFlagImg.style.display = "block"
+    englishFlagImg.style.display = "none"
+}
+else{
+    frenchFlagImg.style.display = "none"
+    englishFlagImg.style.display = "block"
 }
