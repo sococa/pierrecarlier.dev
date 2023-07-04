647605ec9180537552f1959af155ada7f0740c17
sococa <pierrocarlier@gmail.com>
2023-07-04 16:05:03 +0200

    add circle + suppr john doe

--- formulaire.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo 'name : '.$_POST["name"];
-    echo 'email : '.$_POST["email"];
-    echo 'subject : '.$_POST["subject"];
-    echo 'message : '.$_POST["message"];
-?>
--- scripts.js
+++ scripts.js
@@ -1,99 +1,115 @@
+
 /* Gestion du surlignage de la navbar */
 const navItems = document.querySelectorAll(".nav_item");
 let formerId = 0;
 
 const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.6
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.6,
 };
 
-const observerForNavbar = new IntersectionObserver(handleObserverForNavbar, options);
+const observerForNavbar = new IntersectionObserver(
+  handleObserverForNavbar,
+  options
+);
 
 const targets = document.querySelectorAll(".slider_bloc");
 
-targets.forEach(target => {
-    observerForNavbar.observe(target);
-})
+targets.forEach((target) => {
+  observerForNavbar.observe(target);
+});
 
-const underline = id => {
-    navItems[formerId].classList.remove('displayed');
-    navItems[id].classList.add('displayed');
-    formerId = id;
-}
+const underline = (id) => {
+  navItems[formerId].classList.remove("displayed");
+  navItems[id].classList.add("displayed");
+  formerId = id;
+};
 
 function handleObserverForNavbar(entries, observerForNavbar) {
-    entries.forEach(entry => {
-        entry.isIntersecting && underline(entry.target.dataset.id);
-    })
+  entries.forEach((entry) => {
+    entry.isIntersecting && underline(entry.target.dataset.id);
+  });
 }
-
 
 /* Gestion de la arrow_top */
 /* La flèche apparait quand la photo d'accueil n'est plus visible */
-var observerForArrow = new IntersectionObserver(function(entries) {
-    if(entries[0].isIntersecting === true){
-        document.getElementById("arrow_go_to_top").style.display = "none"
+var observerForArrow = new IntersectionObserver(
+  function (entries) {
+    if (entries[0].isIntersecting === true) {
+      document.getElementById("arrow_go_to_top").style.display = "none";
+    } else {
+      document.getElementById("arrow_go_to_top").style.display = "block";
     }
-    else {
-        document.getElementById("arrow_go_to_top").style.display = "block"
-    }
-}, { 
-    threshold: [1] 
-})
+  },
+  {
+    threshold: [1],
+  }
+);
 
-observerForArrow.observe(document.querySelector(".first_part"))
-
+observerForArrow.observe(document.querySelector(".first_part"));
 
 /* Version anglaise ou française */
-let frenchSelected = document.getElementById('french_selected')
-let englishSelected = document.getElementById('english_selected')
+let frenchSelected = document.getElementById("french_selected");
+let englishSelected = document.getElementById("english_selected");
 
-let frenchText = document.querySelectorAll('.french_text')
-let englishText = document.querySelectorAll('.english_text')
+let frenchText = document.querySelectorAll(".french_text");
+let englishText = document.querySelectorAll(".english_text");
 
-let frenchFlagImg = document.getElementById('french_flag_img')
-let englishFlagImg = document.getElementById('english_flag_img')
+let frenchFlagImg = document.getElementById("french_flag_img");
+let englishFlagImg = document.getElementById("english_flag_img");
 
 function languageSelected() {
-    let n = 0;
-    while (n < frenchText.length || n < englishText.length) {
-        
-        if(frenchSelected.checked === true) {
-            frenchText[n].style.display = "block";
-            englishText[n].style.display = "none";
+  let n = 0;
+  while (n < frenchText.length || n < englishText.length) {
+    if (frenchSelected.checked === true) {
+      frenchText[n].style.display = "block";
+      englishText[n].style.display = "none";
 
-            frenchFlagImg.style.display = "none";
-            englishFlagImg.style.display = "block";
-            
-            n++;
-        }
-        else {
-            englishText[n].style.display = "block";
-            frenchText[n].style.display = "none";
+      frenchFlagImg.style.display = "none";
+      englishFlagImg.style.display = "block";
 
-            englishFlagImg.style.display = "none";
-            frenchFlagImg.style.display = "block";
+      n++;
+    } else {
+      englishText[n].style.display = "block";
+      frenchText[n].style.display = "none";
 
-            n++;
-        }
+      englishFlagImg.style.display = "none";
+      frenchFlagImg.style.display = "block";
+
+      n++;
     }
+  }
 }
 
-if(englishSelected.checked === true){
-    frenchFlagImg.style.display = "block";
-    englishFlagImg.style.display = "none";
-}
-else{
-    frenchFlagImg.style.display = "none";
-    englishFlagImg.style.display = "block";
+if (englishSelected.checked === true) {
+  frenchFlagImg.style.display = "block";
+  englishFlagImg.style.display = "none";
+} else {
+  frenchFlagImg.style.display = "none";
+  englishFlagImg.style.display = "block";
 }
 
-if(englishSelected.checked === true){
-    frenchFlagImg.style.display = "block"
-    englishFlagImg.style.display = "none"
+if (englishSelected.checked === true) {
+  frenchFlagImg.style.display = "block";
+  englishFlagImg.style.display = "none";
+} else {
+  frenchFlagImg.style.display = "none";
+  englishFlagImg.style.display = "block";
 }
-else{
-    frenchFlagImg.style.display = "none"
-    englishFlagImg.style.display = "block"
+
+/*EmailJS*/
+function sendMail(params) {
+  let templateParams = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value
+  };
+
+  emailjs.send("service_qy8hu6j", "template_z6lphgo", templateParams)
+  .then(function(res){
+    console.log("success", res.status)
+  })
 }
