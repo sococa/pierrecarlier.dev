c4022ecb894e4d40f3fd682a952080aa658ee6bd
sococa <pierrocarlier@gmail.com>
2023-08-18 12:31:44 +0200

    pagination ok, js terminé

--- scripts.js
+++ scripts.js
@@ -117,5 +117,108 @@ function sendMail(params) {
 }
 
 /* Pagination */
-let numberPerPage = 6;
-let pageNumber = 1;
+const paginationNumbers = document.getElementById("pagination_numbers");
+const paginatedList = document.getElementById("paginated_list");
+const listItems = paginatedList.querySelectorAll("li");
+const nextButton = document.getElementById("next_button");
+const prevButton = document.getElementById("prev_button");
+
+const paginationLimit = 6;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+//Rend les flèches cliquables ou non cliquables
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+//bouton désactivé si pas possible de naviguer vers page précédente ou suivante
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+// mise en évidence du numéro de page actuellement affiché e
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination_number").forEach((button) => {
+    button.classList.remove("active");
+    const pageIndex = Number(button.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      button.classList.add("active");
+    }
+  });
+};
+
+//Crée les numéros de page (button)
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "pagination_number";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+// génère les boutons de pagination en fonction du nombre total de pages et les ajoute au conteneur
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+// permet de définir la page actuelle et met à jour l'affichage des éléments paginés, les boutons de pagination et les boutons "Précédent" et "Suivant".
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  document.querySelectorAll(".pagination_number").forEach((button) => {
+    const pageIndex = Number(button.getAttribute("page-index"));
+
+    if (pageIndex) {
+      button.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
